0ae2a694a7d17714c680f1cda88acad2b4ad905b
Glendon Chin <glendonchin@gmail.com>
2025-02-08 11:17:43 -0500

    configure ts, vite

--- mallards-frontend/vite.config.ts
+++ mallards-frontend/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  server: {
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    postcss: './postcss.config.js'
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  }
+}) 
