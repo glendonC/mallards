94efcfee55b822cc6bb4f00e33ba412e9a49b3d4
Glendon Chin <glendonchin@gmail.com>
2025-02-08 15:57:37 -0500

    update imports

--- mallards-frontend/src/App.tsx
+++ mallards-frontend/src/App.tsx
@@ -6,6 +6,9 @@ import LandingPage from './pages/LandingPage';
 import DataUpload from './pages/DataUpload';
 import DataMapping from './pages/DataMapping';
 import DataConfiguration from './pages/DataConfiguration';
+import Dashboard from './pages/Dashboard';
+import Anomalies from './pages/Anomalies';
+import PredictiveInsights from './pages/PredictiveInsights';
 import ModelSelection from './pages/ModelSelection';
 import "./styles/global.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -21,6 +24,9 @@ const App: React.FC = () => {
             <Route path="/mapping" element={<DataMapping />} />
             <Route path="/configure" element={<DataConfiguration />} />
             <Route path="/models" element={<ModelSelection />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/anomalies" element={<Anomalies />} />
+            <Route path="/predictive" element={<PredictiveInsights />} />
           </Routes>
         </BrowserRouter>
       </DataProvider>
